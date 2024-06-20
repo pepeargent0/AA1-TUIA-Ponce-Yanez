4491335193dfd52b63cbe463e6c96cb9ffa295cf
pepeargentoo <dani_rc_22@hotmail.com>
2024-06-20 20:42:45 -0300

    app progreso

--- app.py
+++ app.py
@@ -1,81 +1,85 @@
-import streamlit as st
-from pathlib import Path
-import json
-from streamlit_extras.switch_page_button import switch_page
-from streamlit.source_util import _on_pages_changed, get_pages
+from flask import Flask, render_template, request, redirect, url_for, flash
+import joblib
+import numpy as np
 
-DEFAULT_PAGE = "main.py"
-SECOND_PAGE_NAME = "dashboard"
+# Cargar los modelos
+#modelo_cuanto_llueve = joblib.load('weather_regression.joblib')
+#modelo_si_llueve = joblib.load('weather_clasificacion.joblib')
 
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
-def get_all_pages():
-    """Get all pages"""
-    default_pages = get_pages(DEFAULT_PAGE)
+# Función ficticia para la autenticación
+def authenticate(username, password):
+    # Aquí puedes agregar la lógica de autenticación
+    return username == "admin" and password == "password"
 
-    pages_path = Path("pages.json")
+@app.route('/')
+def home():
+    return 'pppp'
 
-    if pages_path.exists():
-        saved_default_pages = json.loads(pages_path.read_text())
-    else:
-        saved_default_pages = default_pages.copy()
-        pages_path.write_text(json.dumps(default_pages, indent=4))
-
-    return saved_default_pages
-
-def clear_all_ex_first_page():
-    """Clear all the except Login"""
-    current_pages = get_pages(DEFAULT_PAGE)
-
-    if len(current_pages.keys()) == 1:
-        return
-
-    get_all_pages()
-
-    key, val = list(current_pages.items())[0]
-    current_pages.clear()
-    current_pages[key] = val
-
-    _on_pages_changed.send()
-
-
-def show_all_pages():
-    """Show all Pages"""
-    current_pages = get_pages(DEFAULT_PAGE)
-
-    saved_pages = get_all_pages()
-    for key in saved_pages:
-        if key not in current_pages:
-            current_pages[key] = saved_pages[key]
-
-    _on_pages_changed.send()
-
-def hide_page(name: str):
-    """Hide Default Page"""
-    current_pages = get_pages(DEFAULT_PAGE)
-
-    for key, val in current_pages.items():
-        if val["page_name"] == name:
-            del current_pages[key]
-            _on_pages_changed.send()
-            break
-
-clear_all_ex_first_page()
-def main():
-    st.header("Login")
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-
-    submit = st.button("Login")
-
-    if submit:
-        if username == 'admin' and password == 'admin':
-            st.success("Logged In Sucessful")
-            show_all_pages()
-            hide_page(DEFAULT_PAGE.replace(".py", ""))
-            switch_page(SECOND_PAGE_NAME)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if authenticate(username, password):
+            return redirect(url_for('predict'))
         else:
-            st.error("Invalid Username or Password")
-            clear_all_ex_first_page()
+            flash('Credenciales incorrectas. Por favor, inténtalo de nuevo.')
+    return render_template('login.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        data = request.form
+        example_features = {
+            'MinTemp': float(data['MinTemp']),
+            'MaxTemp': float(data['MaxTemp']),
+            'Rainfall': float(data['Rainfall']),
+            'Evaporation': float(data['Evaporation']),
+            'Sunshine': float(data['Sunshine']),
+            'WindGustSpeed': float(data['WindGustSpeed']),
+            'Pressure9am': float(data['Pressure9am']),
+            'Pressure3pm': float(data['Pressure3pm']),
+            'Temp9am': float(data['Temp9am']),
+            'Temp3pm': float(data['Temp3pm']),
+            'Humidity9am': float(data['Humidity9am']),
+            'Humidity3pm': float(data['Humidity3pm']),
+            'Cloud9am': float(data['Cloud9am']),
+            'Cloud3pm': float(data['Cloud3pm']),
+            'WindSpeed9am': float(data['WindSpeed9am']),
+            'WindSpeed3pm': float(data['WindSpeed3pm']),
+            'RainToday': float(data['RainToday'])
+        }
+
+        data_transform = {
+            'MinTemp': example_features['MinTemp'],
+            'MaxTemp': example_features['MaxTemp'],
+            'Rainfall': example_features['Rainfall'],
+            'Evaporation': example_features['Evaporation'],
+            'Sunshine': example_features['Sunshine'],
+            'WindGustSpeed': example_features['WindGustSpeed'],
+            'PressureVariation': example_features['Pressure3pm'] - example_features['Pressure9am'],
+            'TempVariation': example_features['Temp3pm'] - example_features['Temp9am'],
+            'HumidityVariation': example_features['Humidity3pm'] - example_features['Humidity9am'],
+            'CloudVariation': example_features['Cloud3pm'] - example_features['Cloud9am'],
+            'WindSpeedVariation': example_features['WindSpeed3pm'] - example_features['WindSpeed9am'],
+            'RainToday': example_features['RainToday']
+        }
+
+        features_array = np.array(list(data_transform.values())).reshape(1, -1)
+        #prediccion_llueve = modelo_si_llueve.predict(features_array)
+        prediccion_llueve = 1
+        if prediccion_llueve == 0:
+            result = 'No llueve'
+        else:
+            #predicted_rainfall = modelo_cuanto_llueve.predict(features_array)
+            predicted_rainfall = 0
+            result = f'Llueve: {predicted_rainfall:.2f} mm'
+
+        return render_template('predict.html', result=result)
+    return render_template('predict.html')
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True, host='0.0.0.0', port=5001)
