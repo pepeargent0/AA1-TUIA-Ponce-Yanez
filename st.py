9c2a68993b61773ed7cb40af5c9270bd4f981af4
pepeargentoo <dani_rc_22@hotmail.com>
2024-06-22 02:17:47 -0300

    st feo

--- st.py
+++ st.py
@@ -2,6 +2,7 @@ import streamlit as st
 import joblib
 import numpy as np
 
+# Cargar modelos
 @st.cache_data
 def load_models():
     try:
@@ -45,9 +46,19 @@ def predict(data):
         'Cloud3pm': float(data['Cloud3pm']),
         'WindSpeed9am': float(data['WindSpeed9am']),
         'WindSpeed3pm': float(data['WindSpeed3pm']),
-        'RainToday': float(data['RainToday'])
+        'RainToday': float(data['RainToday']),
+        'WindGustDir': data['WindGustDir'],
+        'WindDir9am': data['WindDir9am'],
+        'WindDir3pm': data['WindDir3pm']
     }
 
+    wind_dir_mapping = {
+        'N': 0, 'NNE': 1, 'NE': 2, 'ENE': 3, 'E': 4, 'ESE': 5, 'SE': 6, 'SSE': 7,
+        'S': 8, 'SSW': 9, 'SW': 10, 'WSW': 11, 'W': 12, 'WNW': 13, 'NW': 14, 'NNW': 15
+    }
+    example_features['WindGustDir'] = wind_dir_mapping[example_features['WindGustDir']]
+    example_features['WindDir9am'] = wind_dir_mapping[example_features['WindDir9am']]
+    example_features['WindDir3pm'] = wind_dir_mapping[example_features['WindDir3pm']]
     data_transform = {
         'MinTemp': example_features['MinTemp'],
         'MaxTemp': example_features['MaxTemp'],
@@ -77,23 +88,28 @@ st.title('Predicción de lluvia en Australia')
 
 if modelo_cuanto_llueve and modelo_si_llueve:
     with st.form(key='prediction_form'):
-        MinTemp = st.number_input('MinTemp', value=0.0)
-        MaxTemp = st.number_input('MaxTemp', value=0.0)
-        Rainfall = st.number_input('Rainfall', value=0.0)
-        Evaporation = st.number_input('Evaporation', value=0.0)
-        Sunshine = st.number_input('Sunshine', value=0.0)
-        WindGustSpeed = st.number_input('WindGustSpeed', value=0.0)
-        Pressure9am = st.number_input('Pressure9am', value=0.0)
-        Pressure3pm = st.number_input('Pressure3pm', value=0.0)
-        Temp9am = st.number_input('Temp9am', value=0.0)
-        Temp3pm = st.number_input('Temp3pm', value=0.0)
-        Humidity9am = st.number_input('Humidity9am', value=0.0)
-        Humidity3pm = st.number_input('Humidity3pm', value=0.0)
-        Cloud9am = st.number_input('Cloud9am', value=0.0)
-        Cloud3pm = st.number_input('Cloud3pm', value=0.0)
-        WindSpeed9am = st.number_input('WindSpeed9am', value=0.0)
-        WindSpeed3pm = st.number_input('WindSpeed3pm', value=0.0)
-        RainToday = st.selectbox('RainToday', options=[0, 1])
+        Location = st.selectbox('Location', ['Adelaide', 'Canberra', 'Cobar', 'Dartmoor', 'Melbourne', 'Melbourne Airport', 'Mount Gambier', 'Sydney', 'Sydney Airport'])
+        MinTemp = st.slider('MinTemp', min_value=-8.5, max_value=33.9, value=19.7, step=0.1)
+        MaxTemp = st.slider('MaxTemp', min_value=-4.8, max_value=48.1, value=21.5, step=0.1)
+        Rainfall = st.slider('Rainfall', min_value=0.0, max_value=371.0, value=0.0, step=0.1)
+        Evaporation = st.slider('Evaporation', min_value=0.0, max_value=145.0, value=13.2, step=0.1)
+        Sunshine = st.slider('Sunshine', min_value=0.0, max_value=14.5, value=1.3, step=0.1)
+        WindGustSpeed = st.slider('WindGustSpeed', min_value=6.0, max_value=135.0, value=67.0, step=1.0)
+        Pressure9am = st.slider('Pressure9am', min_value=980.5, max_value=1041.0, value=1029.6, step=0.1)
+        Pressure3pm = st.slider('Pressure3pm', min_value=977.1, max_value=1039.6, value=1033.6, step=0.1)
+        Temp9am = st.slider('Temp9am', min_value=-7.2, max_value=40.2, value=20.3, step=0.1)
+        Temp3pm = st.slider('Temp3pm', min_value=-5.4, max_value=46.7, value=19.0, step=0.1)
+        Humidity9am = st.slider('Humidity9am', min_value=0.0, max_value=100.0, value=61.0, step=0.1)
+        Humidity3pm = st.slider('Humidity3pm', min_value=0.0, max_value=100.0, value=56.0, step=0.1)
+        Cloud9am = st.slider('Cloud9am', min_value=0.0, max_value=9.0, value=5.0, step=0.1)
+        Cloud3pm = st.slider('Cloud3pm', min_value=0.0, max_value=9.0, value=7.0, step=0.1)
+        WindSpeed9am = st.slider('WindSpeed9am', min_value=0.0, max_value=130.0, value=39.0, step=0.1)
+        WindSpeed3pm = st.slider('WindSpeed3pm', min_value=0.0, max_value=87.0, value=26.0, step=0.1)
+        RainToday = st.selectbox('RainToday', ['No', 'Sí'])
+        RainToday = 1 if RainToday == 'Sí' else 0
+        WindGustDir = st.selectbox('WindGustDir', ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW'])
+        WindDir9am = st.selectbox('WindDir9am', ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW'])
+        WindDir3pm = st.selectbox('WindDir3pm', ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW'])
 
         submit_button = st.form_submit_button(label='Predecir')
 
@@ -115,10 +131,12 @@ if modelo_cuanto_llueve and modelo_si_llueve:
             'Cloud3pm': Cloud3pm,
             'WindSpeed9am': WindSpeed9am,
             'WindSpeed3pm': WindSpeed3pm,
-            'RainToday': RainToday
+            'RainToday': RainToday,
+            'WindGustDir': WindGustDir,
+            'WindDir9am': WindDir9am,
+            'WindDir3pm': WindDir3pm
         }
         result = predict(data)
         st.success(f'¿Es probable que llueva? {result}')
 else:
     st.error("Modelos no cargados correctamente. Por favor, contacte al administrador.")
-
