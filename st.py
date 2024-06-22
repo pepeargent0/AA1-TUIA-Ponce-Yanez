b8cc0c41ae3efeb48c8df73bbfccc54b4df4e039
pepeargentoo <dani_rc_22@hotmail.com>
2024-06-22 02:19:15 -0300

    st feo

--- st.py
+++ st.py
@@ -56,6 +56,7 @@ def predict(data):
         'N': 0, 'NNE': 1, 'NE': 2, 'ENE': 3, 'E': 4, 'ESE': 5, 'SE': 6, 'SSE': 7,
         'S': 8, 'SSW': 9, 'SW': 10, 'WSW': 11, 'W': 12, 'WNW': 13, 'NW': 14, 'NNW': 15
     }
+    
     example_features['WindGustDir'] = wind_dir_mapping[example_features['WindGustDir']]
     example_features['WindDir9am'] = wind_dir_mapping[example_features['WindDir9am']]
     example_features['WindDir3pm'] = wind_dir_mapping[example_features['WindDir3pm']]
